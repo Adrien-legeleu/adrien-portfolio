86b5f74fcb0382ae5ffccc57d677fd5d61b8ee18
Test <adrienlegeleu@gmail.com>
2024-11-05 19:35:56 +0100

    portfolio

---
+++ data/projects-data.ts
@@ -0,0 +1,16 @@
+import project1 from "@/public/image/projects/app-banque.netlify.app_(Nest Hub Max).png";
+import project2 from "@/public/image/projects/le-resto.netlify.app_(Nest Hub Max).png";
+import project3 from "@/public/image/projects/lurex.netlify.app_(Nest Hub Max) (1).png";
+import project4 from "@/public/image/projects/orlumiere.netlify.app_(Nest Hub) (1).png";
+import project5 from "@/public/image/projects/sturmayr.netlify.app_(Nest Hub Max).png";
+import project6 from "@/public/image/projects/sushifood.netlify.app_(Nest Hub Max).png";
+import project7 from "@/public/image/projects/villa-tyklouyou.netlify.app_(Nest Hub Max).png";
+const projectsDat = [
+  {
+    img: project1,
+    title: "Naseem",
+    link: "https://app-banque.netlify.app/",
+    langauges: ["react", "tailwind", "typescript"],
+    description: "",
+  },
+];
